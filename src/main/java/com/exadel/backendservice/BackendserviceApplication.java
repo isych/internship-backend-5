2106d08c6f283240c5017f5312e9d75533dd2b4e
Dmitry Karachun <karachun.dmitry@mail.ru>
2021-03-23 12:41:05 +0300

    add empty template

---
+++ src/main/java/com/exadel/backendservice/BackendserviceApplication.java
@@ -0,0 +1,12 @@
+package com.exadel.backendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendserviceApplication.class, args);
+    }
+}
