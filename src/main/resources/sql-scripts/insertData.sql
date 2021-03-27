ad9c13a5fc159f7acbecf1ad81500a24a69eb105
Dmitry Karachun <karachun.dmitry@mail.ru>
2021-03-27 09:43:58 +0300

    fix bug in insertData.sql

--- src/main/resources/sql-scripts/insertData.sql
+++ src/main/resources/sql-scripts/insertData.sql
@@ -4,4 +4,4 @@ insert into role_table(name) values ('ROLE_ADMIN');
 insert into role_table(name) values ('ROLE_TECH');
 	
 insert into user_table values (999998, 'test-admin@fortest.ru', 'Иванов Иван Иванович', 'test-admin', '$2a$10$dvup/KVkxe/puUu6q8xh8OI2noobcb1dQ1bCjcRVI/P1ovDBlVT4e', 1);
-into into user_table values (999999, 'test-user@fortest.ru', 'Петров Петр Петрович', 'test-user', '$2a$10$dvup/KVkxe/puUu6q8xh8OI2noobcb1dQ1bCjcRVI/P1ovDBlVT4e', 2);
+insert into user_table values (999999, 'test-user@fortest.ru', 'Петров Петр Петрович', 'test-user', '$2a$10$dvup/KVkxe/puUu6q8xh8OI2noobcb1dQ1bCjcRVI/P1ovDBlVT4e', 2);
