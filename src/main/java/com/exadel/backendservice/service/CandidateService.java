656f70ee0601abc6833d7d5b113c2ab3b419ec31
drevenb <drevenb@gmail.com>
2021-04-13 18:48:52 +0300

    Add getting candidate statuses, interview statuses and preferred time

--- src/main/java/com/exadel/backendservice/service/CandidateService.java
+++ src/main/java/com/exadel/backendservice/service/CandidateService.java
@@ -7,8 +7,16 @@ import com.exadel.backendservice.entity.Candidate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface CandidateService {
     Candidate registerCandidate(RegisterCandidateDto candidateDto);
 
     Page<SearchCandidateDto> getPageOfCandidates(Pageable pageable);
+
+    List<String> getAllStatuses();
+
+    List<String> getInterviewStatuses();
+
+    List<String> getAllPreferredTime();
 }
--- src/main/java/com/exadel/backendservice/service/impl/CandidateServiceImpl.java
+++ src/main/java/com/exadel/backendservice/service/impl/CandidateServiceImpl.java
@@ -5,6 +5,9 @@ import com.exadel.backendservice.dto.resp.SearchCandidateDto;
 import com.exadel.backendservice.entity.Candidate;
 import com.exadel.backendservice.mapper.converter.RegisterCandidateMapper;
 import com.exadel.backendservice.mapper.converter.SearchCandidateMapper;
+import com.exadel.backendservice.model.CandidateStatus;
+import com.exadel.backendservice.model.InterviewProcess;
+import com.exadel.backendservice.model.PreferredTime;
 import com.exadel.backendservice.repository.CandidateRepository;
 import com.exadel.backendservice.service.CandidateService;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @Slf4j
@@ -50,5 +54,29 @@ public class CandidateServiceImpl implements CandidateService {
         Page<Candidate> page = candidateRepository.findAll(pageable);
         List<SearchCandidateDto> candidateDtoList = page.get().map(searchCandidateMapper::toDto).collect(Collectors.toList());
         return new PageImpl<>(candidateDtoList);
+    }
+
+    @Override
+    public List<String> getAllStatuses() {
+        List<String> statusList = Stream.of(CandidateStatus.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return statusList;
+    }
+
+    @Override
+    public List<String> getInterviewStatuses() {
+        List<String> interviewStatusesList = Stream.of(InterviewProcess.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return interviewStatusesList;
+    }
+
+    @Override
+    public List<String> getAllPreferredTime() {
+        List<String> preferredTimesList = Stream.of(PreferredTime.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return preferredTimesList;
     }
 }
