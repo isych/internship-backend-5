2b44b44feb629f09f60601f9bc37996c71e12e19
Dmitry Karachun <karachun@nca.by>
2021-04-16 17:28:05 +0300

    add tests for user controller methods

--- src/test/java/com/exadel/backendservice/config/AbstractTestConfig.java
+++ src/test/java/com/exadel/backendservice/config/AbstractTestConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -18,8 +19,10 @@ public class AbstractTestConfig {
     @Container
     static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:12.6-alpine");
 
+
     @DynamicPropertySource
     static void registerDynamicProperties(DynamicPropertyRegistry registry) {
         ApplicationTestPropertyValues.populateRegistryFromPostgresContainer(registry, postgreSQLContainer);
     }
+
 }
