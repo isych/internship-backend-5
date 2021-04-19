42dd648b6e8b1e97929688f66e96fa9e9c8ba3cb
Dmitry Karachun <karachun@nca.by>
2021-04-19 15:04:31 +0300

    add tests for LocationController and TechController

--- src/test/java/com/exadel/backendservice/config/AbstractTestConfig.java
+++ src/test/java/com/exadel/backendservice/config/AbstractTestConfig.java
@@ -1,9 +1,11 @@
 package com.exadel.backendservice.config;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
