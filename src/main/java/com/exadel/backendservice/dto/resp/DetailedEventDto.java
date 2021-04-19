9c8aedad12f5e4899a354e4055e58430a707a987
Dmitry Karachun <karachun@nca.by>
2021-04-19 11:08:17 +0300

    task HUM_57 completed

--- src/main/java/com/exadel/backendservice/dto/resp/DetailedEventDto.java
+++ src/main/java/com/exadel/backendservice/dto/resp/DetailedEventDto.java
@@ -2,13 +2,11 @@ package com.exadel.backendservice.dto.resp;
 
 import com.exadel.backendservice.dto.AbstractDto;
 import com.exadel.backendservice.dto.LocationDto;
-import com.exadel.backendservice.entity.City;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
