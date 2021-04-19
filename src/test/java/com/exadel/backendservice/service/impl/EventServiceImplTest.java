04c11bb940cca48e7f24067237e01bb369cca5a4
Dmitry Karachun <karachun@nca.by>
2021-04-19 11:56:36 +0300

    add tests for userSerice

---
+++ src/test/java/com/exadel/backendservice/service/impl/EventServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.exadel.backendservice.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventServiceImplTest {
+
+    @Test
+    void saveEvent() {
+    }
+
+    @Test
+    void getEventTypes() {
+    }
+
+    @Test
+    void getEventsPage() {
+    }
+
+    @Test
+    void getEvent() {
+    }
+
+    @Test
+    void isUnique() {
+    }
+}
