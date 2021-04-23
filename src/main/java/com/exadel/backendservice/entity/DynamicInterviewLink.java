3297ddef9520659d94c1161f4c1f28e85ae4ba1f
dmitry <dmytro.bolhov@gmail.com>
2021-04-23 14:33:27 +0300

    change DB 2

---
+++ src/main/java/com/exadel/backendservice/entity/DynamicInterviewLink.java
@@ -0,0 +1,29 @@
+package com.exadel.backendservice.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "dynamic_interview_link")
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class DynamicInterviewLink extends AbstractEntity {
+
+    @Column(nullable = false)
+    private String code;
+
+    @Column(name = "created_time", nullable = false)
+    private LocalDateTime createdTime;
+
+    @Column(name = "interview_id", nullable = false)
+    private Integer interviewId;
+}
