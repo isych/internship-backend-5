4e5633113e8c7401317d6fd5e9e46b896dbf6a5f
dmitry <dmytro.bolhov@gmail.com>
2021-04-23 14:45:56 +0300

    1314

---
+++ src/main/resources/db/migration/V1__drop_tables_if_exists.sql
@@ -0,0 +1,16 @@
+drop table if exists users cascade;
+drop table if exists roles cascade;
+drop table if exists event_label cascade;
+drop table if exists labels cascade;
+drop table if exists candidate cascade;
+drop table if exists event_stack cascade;
+drop table if exists event_city cascade;
+drop table if exists events cascade;
+drop table if exists city cascade;
+drop table if exists country cascade;
+drop sequence if exists roles_id_seq cascade;
+drop sequence if exists users_id_seq cascade;
+drop sequence if exists hibernate_sequence cascade;
+
+
+
