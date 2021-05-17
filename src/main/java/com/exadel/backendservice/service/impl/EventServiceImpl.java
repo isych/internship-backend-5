53105d684f0af377b4be98b87b326b7b268cf64a
dmitry <dmytro.bolhov@gmail.com>
2021-05-17 16:51:24 +0300

    change request param on path variable

--- src/main/java/com/exadel/backendservice/service/impl/EventServiceImpl.java
+++ src/main/java/com/exadel/backendservice/service/impl/EventServiceImpl.java
@@ -309,7 +309,7 @@ public class EventServiceImpl implements EventService {
         Map<String, Object> info = new HashMap<>();
         Set<String> tech = new HashSet<>();
         Set<String> countries = new HashSet<>();
-        Set<EventType> type = events.stream().map(elem -> elem.getType()).collect(Collectors.toSet());
+        Set<EventType> type = events.stream().map(Event::getType).collect(Collectors.toSet());
         Set<EventStatus> eventStatuses = events.stream().map(Event::getEventStatus).collect(Collectors.toSet());
         events.stream().map(elem -> elem.getTechs().stream().map(Tech::getName).collect(Collectors.toSet())).forEach(tech::addAll);
         events.stream().map(elem -> elem.getCities().stream().map(el -> el.getCountry().getName()).collect(Collectors.toSet())).forEach(countries::addAll);
@@ -318,5 +318,13 @@ public class EventServiceImpl implements EventService {
         info.put("status", eventStatuses);
         info.put("type", type);
         return info;
+    }
+
+    @Override
+    public EventRespDto editEvent(UUID id, CreateEventDto dto) {
+        Event event = createEventMapper.toEntity(dto);
+        event.setId(id);
+        Event eventWithID = eventRepository.save(event);
+        return eventResponseMapper.toDto(eventWithID);
     }
 }
